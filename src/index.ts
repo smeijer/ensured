3373668ba0dc2faf95ecc057cf23efc9f3920bde
Stephan Meijer <stephan.meijer@gmail.com>
2025-11-11 13:26:58 +0100

    feat: assert, expect, unreachable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-export function main(offset: number) {
-	if (typeof offset !== 'number') {
-		throw new TypeError(`Expected a number, got ${typeof offset}`);
-	}
+const defaultMessage: string = 'Invariant failed' as const;
+type ErrorParam = Error | string | (() => Error | string);
 
-	return Date.now() + offset;
+function toError(input: ErrorParam | undefined): Error {
+	if (input === undefined) return new Error(defaultMessage);
+	const v = typeof input === 'function' ? input() : input;
+	return typeof v === 'string' ? new Error(v) : v;
+}
+
+export function assert(condition: unknown, error?: ErrorParam): asserts condition {
+	if (condition) return;
+	throw toError(error);
+}
+
+export function expect<Value>(value: Value | null | undefined, error?: ErrorParam): Value {
+	assert(value != null, error);
+	return value as NonNullable<Value>;
+}
+
+export function unreachable(error?: ErrorParam): never;
+export function unreachable(x: never, error?: ErrorParam): never;
+export function unreachable(a?: unknown, b?: ErrorParam): never {
+	if (arguments.length === 1) {
+		throw toError(a as ErrorParam);
+	}
+	throw toError(b ?? `Unreachable reached: ${String(a)}`);
 }
